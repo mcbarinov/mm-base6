730f1865bdb892c7a65b7bc0ce1e349397eeeb8a
mcbarinov <mcbarinov@proton.me>
2025-03-21 09:11:46 +0300

    serve_uvicorn

--- src/app/main.py
+++ src/app/main.py
@@ -1,26 +1,18 @@
-import asyncio
-
-import uvicorn
+import anyio
 
 from app import settings
 from app.core.core import Core
 from app.server.jinja import custom_jinja
 from mm_base6 import init_server
+from mm_base6.server.uvicorn import serve_uvicorn
 
 
 async def main() -> None:
     core = await Core.init(settings.core_config)
     await core.startup()
     fastapi_app = init_server(core, settings.server_config, custom_jinja, settings.get_router())
-
-    config = uvicorn.Config(fastapi_app, host="0.0.0.0", port=3000, log_level="warning")  # noqa: S104 # nosec
-    server = uvicorn.Server(config)
-
-    try:
-        await server.serve()
-    except Exception as e:
-        print(str(e))
+    await serve_uvicorn(fastapi_app, host="0.0.0.0", port=3000, log_level="warning")  # noqa: S104 # nosec
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    anyio.run(main)
